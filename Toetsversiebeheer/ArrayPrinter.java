7d1154354b71af500d8d551e0fc94bcd28c452ab
reyckmans <robbee14@gmail.com>
2019-11-26 10:06:31 +0100

    first commit

---
+++ Toetsversiebeheer/ArrayPrinter.java
@@ -0,0 +1,15 @@
+package Toetsversiebeheer;
+
+public class ArrayPrinter {
+	
+	
+	public static void main(String[]args) {
+		ArrayPrinter array = new ArrayPrinter();
+		int[] a = {4,5,5,6,8};
+		array.printarray(a);
+	}
+	
+	public void printarray(int[] a){
+	}
+
+}
